deb92444fd96985aef7c624e0f9208ab83ce33be
AndreaBononi <andrea.bononi@yahoo.it>
2023-04-21 20:03:35 +0200

    error fixing

--- fpga-maste/AvalonMM_hyperRamS27KL0641_interface/AvalonMM_hyperRamS27KL0641_interface_bb.v
+++
@@ -1,8 +0,0 @@
-
-module AvalonMM_hyperRamS27KL0641_interface (
-	clk_clk,
-	reset_reset_n);	
-
-	input		clk_clk;
-	input		reset_reset_n;
-endmodule
--- fpga-maste/AvalonMM_hyperRamS27KL0641_interface/AvalonMM_hyperRamS27KL0641_interface_inst.v
+++
@@ -1,5 +0,0 @@
-	AvalonMM_hyperRamS27KL0641_interface u0 (
-		.clk_clk       (<connected-to-clk_clk>),       //   clk.clk
-		.reset_reset_n (<connected-to-reset_reset_n>)  // reset.reset_n
-	);
-
