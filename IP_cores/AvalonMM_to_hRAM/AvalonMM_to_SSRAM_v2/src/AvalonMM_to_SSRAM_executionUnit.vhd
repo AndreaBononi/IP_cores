0e9c9f900378e6d835b717b07ef5f9cb1cfb9d5b
AndreaBononi <andrea.bononi@yahoo.it>
2023-05-21 19:16:55 +0200

    new version of AvalonMM to SSRAM vhdl

--- IP_cores/AvalonMM_to_hRAM/AvalonMM_to_SSRAM_v2/src/AvalonMM_to_SSRAM_executionUnit.vhd
+++ IP_cores/AvalonMM_to_hRAM/AvalonMM_to_SSRAM_v2/src/AvalonMM_to_SSRAM_executionUnit.vhd
@@ -8,12 +8,10 @@ use IEEE.numeric_std.all;
 entity AvalonMM_to_SSRAM_executionUnit is
 	generic
 	(
-		-- configuration register 0 (dpd_mode_enable and fixed_latency_enable ar not present, since they can be modified run time)
 		burst_lenght									: std_logic_vector(1 downto 0) := "11";
 		hybrid_burst_enable						: std_logic := '1';
 		initial_latency								: std_logic_vector(3 downto 0) := "0001";
 		drive_strength								: std_logic_vector(2 downto 0) := "000";
-		-- configuration register 1
 		distributed_refresh_interval	: std_logic_vector(1 downto 0) := "10"
 	);
 	port
@@ -46,7 +44,7 @@ entity AvalonMM_to_SSRAM_executionUnit is
 		fifo4_full							: out		std_logic;
 		fifo4_almost_full				: out		std_logic;
 		fifo4_empty							: out		std_logic;
-		dpd_mode_n							: out 	std_logic;
+		dpd_mode								: out 	std_logic;
 		op_req									: out		std_logic;
 		previous_op_req					: out		std_logic;
 		write_op								: out 	std_logic;
@@ -58,10 +56,10 @@ entity AvalonMM_to_SSRAM_executionUnit is
 		command_enable					: in		std_logic;
 		virtual_config_enable		: in		std_logic;
 		virtual_config_clear_n	: in		std_logic;
-		muxout_sel							: in		std_logic;
-		config_reg_sel					: in		std_logic;
+		out_sel									: in		std_logic;
+		config_sel							: in		std_logic;
 		mem_input_sel						: in		std_logic;
-		mem_address_space				: in		std_logic;
+		address_space_sel				: in		std_logic;
 		por_enable							: in		std_logic;
 		por_clear_n							: in		std_logic;
 		tgl_clear_n							: in		std_logic;
@@ -76,6 +74,9 @@ end AvalonMM_to_SSRAM_executionUnit;
 -----------------------------------------------------------------------------------------------------------------------------------
 
 architecture rtl of AvalonMM_to_SSRAM_executionUnit is
+
+	constant config0_addr: std_logic_vector(31 downto 0) := "00000000000000000000100000000000";
+	constant config1_addr: std_logic_vector(31 downto 0) := "00000000000000000000100000000001";
 
 	-- register --------------------------------------------------------------------------------------------------------------------
 	component reg is
@@ -157,9 +158,12 @@ architecture rtl of AvalonMM_to_SSRAM_executionUnit is
 	signal cmd_out								: std_logic_vector(49 downto 0);
 	signal virtual_config_in			: std_logic_vector(15 downto 0);
 	signal virtual_config_out			: std_logic_vector(15 downto 0);
-	signal config_reg_mux_out			: std_logic_vector(15 downto 0);
+	signal config_mux_in0					: std_logic_vector(15 downto 0);
+	signal config_mux_in1					: std_logic_vector(15 downto 0);
+	signal config_mux_out					: std_logic_vector(15 downto 0);
 	signal config_addr_mux_out		: std_logic_vector(15 downto 0);
 	signal address_space_mux_out	: std_logic_vector(31 downto 0);
+	signal out_mux_in0						: std_logic_vector(15 downto 0);
 	signal fifo4_out							: std_logic_vector(49 downto 0);
 	signal fifo4_empty_local			: std_logic;
 	signal tgl_in									: std_logic;
@@ -168,16 +172,53 @@ architecture rtl of AvalonMM_to_SSRAM_executionUnit is
 
 	begin
 
-		cmd_in(31 downto 0) <= avs_s0_address;
-		cmd_in(47 downto 32) <= avs_s0_writedata;
-		cmd_in(48) <= avs_s0_read;
-		cmd_in(49) <= avs_s0_write;
+		cmd_in(49) 						<= avs_s0_write;
+		cmd_in(48) 						<= avs_s0_read;
+		cmd_in(47 downto 32) 	<= avs_s0_writedata;
+		cmd_in(31 downto 0) 	<= avs_s0_address;
 
-		-- command register ----------------------------------------------------------------------------------------------------------
-		command: reg generic map (50) port map (clk, command_enable, '1', cmd_in, cmd_out);
+		COMMAND: reg generic map (50) port map (clk, command_enable, '1', cmd_in, cmd_out);
 
-		-- fifo4 ---------------------------------------------------------------------------------------------------------------------
-		local_fifo: fifo4 generic map (50)
+		write_op <= cmd_out(49);
+
+		op <= avs_s0_read or avs_s0_write;
+		op_req <= op;
+		previous_op_req <= por_out;
+
+		POR: d_flipflop port map (clk, por_enable, por_clear_n, op, por_out);
+
+		config_reg_access <=
+			not avs_s0_address(22) nand avs_s0_address(21) nand avs_s0_address(20) nand avs_s0_address(19)
+			nand avs_s0_address(18) nand avs_s0_address(17) nand avs_s0_address(16) nand avs_s0_address(15)
+			nand avs_s0_address(14) nand avs_s0_address(13) nand avs_s0_address(12) nand avs_s0_address(11)
+			nand avs_s0_address(10) nand avs_s0_address(9) nand avs_s0_address(8) nand avs_s0_address(7)
+			nand avs_s0_address(6) nand avs_s0_address(5) nand avs_s0_address(4) nand avs_s0_address(3)
+			nand avs_s0_address(2) nand avs_s0_address(1) nand avs_s0_address(0)
+		;
+
+		virtual_config_in(15 downto 0) 	<= "00000000000000";
+		virtual_config_in(1 downto 0) 	<= cmd_out(33 downto 32);
+
+		VIRTUAL_CONFIG: reg generic map (16) port map (clk, virtual_config_enable, virtual_config_clear_n, virtual_config_in, virtual_config_out);
+
+		config_mux_in0(15)						<= not(virtual_config_out(0));
+		config_mux_in0(14 downto 12)	<= drive_strength;
+		config_mux_in0(11 downto 8)		<= "1111";
+		config_mux_in0(7 downto 4) 		<= initial_latency;
+		config_mux_in0(3)							<= virtual_config_out(1);
+		config_mux_in0(2) 						<= hybrid_burst_enable;
+		config_mux_in0(1 downto 0) 		<= burst_lenght;
+
+		config_mux_in1(1 downto 0)		<= distributed_refresh_interval;
+		config_mux_in1(15 downto 2)		<= "00000000000000";
+
+		CONFIG_MUX: mux_2to1 generic map (16) port map (config_mux_in0, config_mux_in1, config_sel, config_mux_out);
+
+		OUT_MUX: mux_2to1 generic map (16) port map (out_mux_in0, virtual_config_out, out_sel, avs_s0_readdata);
+
+		READDATA: reg generic map (16) port map (clk, readdata_enable, '1', ssram_out, out_mux_in0);
+
+		LOCAL__FIFO: fifo4 generic map (50)
 		port map
 		(
 			clk, 								-- fifo4_clk
@@ -193,29 +234,24 @@ architecture rtl of AvalonMM_to_SSRAM_executionUnit is
 
 		fifo4_empty <= fifo4_empty_local;
 		tgl_in <= ssram_busy nor fifo4_empty_local;
-		ssram_address <= fifo4_out(31 downto 0);
-		ssram_in <= fifo4_out(47 downto 32);
 		ssram_OE <= fifo4_out(48) and mem_enable;
-		ssram_WE <= fifo4_out(49) and mem_enable;
+		ssram_WE <= (fifo4_out(49) and mem_enable) or force_write;
 		ssram_clear_n <= rst_n;
 		mem_busy <= ssram_busy;
 
-		-- TGL flip flop -------------------------------------------------------------------------------------------------------------
-		tgl: t_flip_flop port map (clk, '1', tgl_clear_n, tgl_in, mem_avail);
+		CONFIG_ADDR_MUX: mux_2to1 generic map (32) port map (config0_addr, config1_addr, config_sel, config_addr_mux_out);
+
+		ADDRESS_SPACE_MUX: mux_2to1 generic map (32) port map (fifo4_out(31 downto 0), config_addr_mux_out, address_space_sel, ssram_address);
+
+		MEM_INPUT_MUX: mux_2to1 generic map (16) port map (fifo4_out(47 downto 32), config_mux_out, mem_input_sel, ssram_in);
+
+		TGL: t_flip_flop port map (clk, '1', tgl_clear_n, tgl_in, mem_avail);
 
 		avs_s0_readdatavalid <= readdatavalid;
 		avs_s0_waitrequest <= waitrequest;
 		mem_validout <= ssram_validout;
-
-		op <= avs_s0_read or avs_s0_write;
-		op_req <= op;
-		previous_op_req <= por_out;
-
-		-- POR flip flop -------------------------------------------------------------------------------------------------------------
-		por: d_flipflop port map (clk, por_enable, por_clear_n, op, por_out);
-
-		-- readdata register ----------------------------------------------------------------------------------------------------------
-		readdata: reg generic map (16) port map (clk, readdata_enable, '1', ssram_out, avs_s0_readdata);
+		ssram_address_space <= address_space_sel;
+		dpd_mode <= virtual_config_out(0);
 
 end architecture rtl;
 
